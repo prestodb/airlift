7062495c3dd8bf4a530b969cf8e2d5f920fed3db
Shahim Sharafudeen <33122287+shahimsharafudeen@users.noreply.github.com>
2025-11-14 21:21:34 +0530

    feat(server): Add a KeyStoreScanner
    To allow for hot reloading the tls keystore
    
    See for more details : https://jetty.org/docs/jetty/12.1/programming-guide/server/http.html#connector-protocol-tls-keystore-auto-reload
    
    Co-authored-by: Shahim S <33122287+shahimsharafudeen@users.noreply.github.com>
    Co-authored-by: Anant Aneja <1797669+aaneja@users.noreply.github.com>

--- http-server/src/test/resources/clientcert-java/clientcert.sh
+++ http-server/src/test/resources/clientcert-java/clientcert.sh
@@ -26,3 +26,23 @@ openssl pkcs12 -name client -inkey client.key -in client.crt -export -passout pa
 # create client truststore
 keytool -import -noprompt -alias ca -file ca.crt -storetype pkcs12 -storepass airlift -keystore client.truststore
 
+
+#A kesytore for testing the KeyStoreScanner's ability to transparently update the ssl config without needing a restart
+
+# create replacement server cert
+openssl req -new -key server.key -subj "/C=US/ST=CA/L=Palo Alto/O=Replacement/OU=Server/CN=localhost" -out replacementServer.csr
+openssl x509 -req -days 9999 -in replacementServer.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out replacementServer.crt
+
+# create replacement server keystore
+openssl pkcs12 -name server -inkey server.key -in replacementServer.crt -export -passout pass:airlift -out replacementServer.keystore
+keytool -import -noprompt -alias ca -file ca.crt -storetype pkcs12 -storepass airlift -keystore replacementServer.keystore
+
+# create client cert
+openssl req -new -key client.key -subj "/C=US/ST=CA/L=Palo Alto/O=Replacement/OU=Client/CN=testing" -out replacementClient.csr
+openssl x509 -req -days 9999 -in replacementClient.csr -CA ca.crt -CAkey ca.key -set_serial 02 -out replacementClient.crt
+
+# create replacement client keystore
+openssl pkcs12 -name client -inkey client.key -in replacementClient.crt -export -passout pass:airlift -out replacementClient.keystore
+
+# create replacement client truststore
+keytool -import -noprompt -alias ca -file ca.crt -storetype pkcs12 -storepass airlift -keystore replacementClient.truststore
